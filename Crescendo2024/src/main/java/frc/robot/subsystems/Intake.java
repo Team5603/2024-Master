7b4b62159c25cc7d3c1a94035013b65c4c2c4aba
Team5603 <team5603programming@gmail.com>
2024-02-17 14:25:08 -0500

    Implemented beam break sensor

--- Crescendo2024/src/main/java/frc/robot/subsystems/Intake.java
+++ Crescendo2024/src/main/java/frc/robot/subsystems/Intake.java
@@ -3,15 +3,19 @@ package frc.robot.subsystems;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.constants.GeneralConstants;
 
 public class Intake extends SubsystemBase {
     CANSparkMax intake, intakeLift;
+    DigitalInput beamBreakSensor;
     public Intake () {
         // placeholder values !!!!!!
-        intake = new CANSparkMax(4, MotorType.kBrushless);
-        intakeLift = new CANSparkMax(5, MotorType.kBrushless);
+        intake = new CANSparkMax(/*placeholder*/4, MotorType.kBrushless);
+        intakeLift = new CANSparkMax(/*placeholder*/5, MotorType.kBrushless);
+
+        beamBreakSensor = new DigitalInput(/*placeholder*/ 6);
     }
     @Override
     public void periodic() {
@@ -40,4 +44,12 @@ public class Intake extends SubsystemBase {
     public double getLiftEncoder() {
         return intakeLift.getEncoder().getPosition();
     }
+
+      public boolean getBeamBroken() {
+    if (beamBreakSensor.get()) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
